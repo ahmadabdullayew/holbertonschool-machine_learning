29df519b0fb2d776eae68b0e4c8e6721a42f3c35
ahmadabdullayew <ahmadabdullayevcontact@gmail.com>
2026-01-25 18:57:02 +0000

    Add polynomial integral function

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,36 +1,26 @@
 #!/usr/bin/env python3
-"""
-Module that calculates the integral of a polynomial
-"""
+"""calculates the integral of a polynomial"""
 
 
 def poly_integral(poly, C=0):
-    """
-    Calculates the integral of a polynomial
-
-    Args:
-        poly (list): list of coefficients
-        C (int): integration constant
-
-    Returns:
-        list: coefficients of the integral polynomial
-    """
-    if not isinstance(poly, list) or not isinstance(C, int):
+    """calculates the integral of a polynomial"""
+    if (not isinstance(poly, list)
+            or any(not isinstance(x, (int, float)) for x in poly)
+            or len(poly) == 0):
         return None
 
-    if len(poly) == 0:
+    if not isinstance(C, int):
         return None
 
     result = [C]
 
     for i in range(len(poly)):
-        coef = poly[i]
-        if not isinstance(coef, (int, float)):
-            return None
-        result.append(coef / (i + 1))
+        coefficient = poly[i] / (i+1)
+        if coefficient.is_integer():
+            coefficient = int(coefficient)
+        result.append(coefficient)
 
+    # sondaki 0 lari silirik
     while len(result) > 1 and result[-1] == 0:
         result.pop()
-
     return result
-

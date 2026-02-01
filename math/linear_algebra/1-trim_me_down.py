b722bf8e85f97ef5ad0c805d8b09480bb9c13d53
ahmadabdullayew <ehmedmanafoghlu@gmail.com>
2026-02-01 14:40:00 +0400

    Commit

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
-n = len(matrix[0])
 for i in range(len(matrix)):
-    the_middle.append([matrix[i][n // 2 - 1], matrix[i][n // 2]])
+    the_middle.append([matrix[i][len(matrix[0]) // 2 - 1], matrix[i][len(matrix[0]) // 2]])
 print("The middle rows of the matrix are: {}".format(the_middle))

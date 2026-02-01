b30a4d45b2a807c3c853a5d91e45ec5b4192d116
ahmadabdullayew <ehmedmanafoghlu@gmail.com>
2026-02-01 19:40:23 +0400

    Commit

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""a function def cat_arrays(arr1, arr2):
+ that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """a function def cat_arrays(arr1, arr2):
+     that concatenates two arrays"""
+    return arr1 + arr2

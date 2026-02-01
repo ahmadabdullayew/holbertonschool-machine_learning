7b0e14edbe0f938f17dc12c9e8481cd3b403d456
ahmadabdullayew <ehmedmanafoghlu@gmail.com>
2026-02-01 16:12:48 +0400

    Commit

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """a function def matrix_shape(matrix):
-that calculates the shape of a matrix
-"""
+ that calculates the shape of a matrix"""
 
 
 def matrix_shape(matrix):
-    """Calculates the shape of a matrix"""
+    """a function def matrix_shape(matrix):
+     that calculates the shape of a matrix"""
     matrix_shape = []
     while isinstance(matrix, list):
         matrix_shape.append(len(matrix))

67ae7712e7e5f56f434949ce2e068358230ff033
ahmadabdullayew <ehmedmanafoghlu@gmail.com>
2026-02-08 23:22:58 +0400

    5-definiteness.py

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """a function def definiteness(matrix):
- that calculates the definiteness of a matrix"""
+that calculates the definiteness of a matrix"""
 import numpy as np
 
 
